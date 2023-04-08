6bda1aaced26c394992823f5acb1ce8307b6dff3
exslym <76844562+exslym@users.noreply.github.com>
2023-04-08 16:00:17 +0400

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,60 @@
 import React, { useState } from 'react';
 import './App.scss';
 
-function App_() {
-	const [player1Counter, setPlayer1Counter] = useState(10);
-	const [player2Counter, setPlayer2Counter] = useState(10);
+// function App_() {
+// 	const [player1Counter, setPlayer1Counter] = useState(() => 10);
+// 	const [player2Counter, setPlayer2Counter] = useState(() => 10);
 
-	return (
-		<div className='App'>
-			<div className='container'>
-				<div className='flexbox'>
-					<div>
-						<div>Player 1</div>
-						<div>Score: {player1Counter}</div>
-						<button
-							onClick={() => {
-								setPlayer1Counter(actual => actual + 1);
-							}}
-						>
-							+
-						</button>
-					</div>
-					<div>
-						<div>Player 2</div>
-						<div>Score: {player2Counter}</div>
-						<button
-							onClick={() => {
-								setPlayer1Counter(actual => actual + 1);
-							}}
-						>
-							+
-						</button>
-					</div>
-				</div>
-				<button
-					onClick={() => {
-						setPlayer1Counter(actual => actual - 1);
-						setPlayer2Counter(actual => actual - 1);
-					}}
-				>
-					-
-				</button>
-				<br />
-				<button
-					onClick={() => {
-						setPlayer1Counter(10);
-						setPlayer2Counter(10);
-					}}
-					className='buttonReset'
-				>
-					Reset
-				</button>
-			</div>
-		</div>
-	);
-}
+// 	return (
+// 		<div className='App'>
+// 			<div className='container'>
+// 				<div className='flexbox'>
+// 					<div>
+// 						<div>Player 1</div>
+// 						<div>Score: {player1Counter}</div>
+// 						<button
+// 							onClick={() => {
+// 								setPlayer1Counter(actual => actual + 1);
+// 							}}
+// 						>
+// 							+
+// 						</button>
+// 					</div>
+// 					<div>
+// 						<div>Player 2</div>
+// 						<div>Score: {player2Counter}</div>
+// 						<button
+// 							onClick={() => {
+// 								setPlayer1Counter(actual => actual + 1);
+// 							}}
+// 						>
+// 							+
+// 						</button>
+// 					</div>
+// 				</div>
+// 				<button
+// 					onClick={() => {
+// 						setPlayer1Counter(actual => actual - 1);
+// 						setPlayer2Counter(actual => actual - 1);
+// 					}}
+// 				>
+// 					-
+// 				</button>
+// 				<br />
+// 				<button
+// 					onClick={() => {
+// 						setPlayer1Counter(10);
+// 						setPlayer2Counter(10);
+// 					}}
+// 					className='buttonReset'
+// 				>
+// 					Reset
+// 				</button>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
 function App() {
 	const [counters, setCounters] = useState({
 		c1: 10,
